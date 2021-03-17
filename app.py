6643f66d5773cb799f5096ff0aeb1e4a5931d929
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-03-17 13:59:19 +0700

    new config with flask-dance

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, url_for, \
     session, g, flash, render_template
-from flask_oauthlib.client import OAuth
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 import config
 
 SECRET_KEY='ajsa3fae841asdryh1adaf4f'
@@ -9,59 +9,60 @@ DEBUG = True
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
-oauth = OAuth()
+app.register_blueprint(twitter_blueprint)
+# oauth = OAuth()
 
-twitter = oauth.remote_app('twitter',
-        base_url='https://api.twitter.com/1/',
-        request_token_url='https://api.twitter.com/aouth/request_token',
-        access_token='https://api.twitter.com/aouth/access_token',
-        authorize_url='https://api.twitter.com/oauth/authenticate',
-        consumer_key=config.CONSUMER_KEY,
-        consumer_secret=config.CONSUMER_SECRET,
-        access_token_method = 'GET')
+# twitter = oauth.remote_app('twitter',
+#         base_url='https://api.twitter.com/1/',
+#         request_token_url='https://api.twitter.com/aouth/request_token',
+#         access_token='https://api.twitter.com/aouth/access_token',
+#         authorize_url='https://api.twitter.com/oauth/authenticate',
+#         consumer_key=config.CONSUMER_KEY,
+#         consumer_secret=config.CONSUMER_SECRET,
+#         access_token_method = 'GET')
 
-@twitter.tokengetter
-def get_twitter_token(token=None):
-    return session.get('twitter_token')
+# @twitter.tokengetter
+# def get_twitter_token(token=None):
+#     return session.get('twitter_token')
+
 
 @app.route('/')
 def index():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('login'))
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login")) # How to handle this?
+    resp = twitter.get("account/settings.json")
+    assert resp.ok
+    return {"screen_name",resp.json()["screen_name"]}
 
-    access_token = access_token[0]
-    return render_template('index.html')
+# @app.route('/login')
+# def login():
+#     return twitter.authorize(callback=url_for('oauth_authorized',
+#         next=request.args.get('next') or request.referrer or None))
 
-@app.route('/login')
-def login():
-    return twitter.authorize(callback=url_for('oauth_authorized',
-        next=request.args.get('next') or request.referrer or None))
+# @app.route('/logout')
+# def logout():
+#     session.pop('screen_name', None)
+#     flash(u'You were signed out')
+#     return redirect(request.referrer or url_for('index'))
 
-@app.route('/logout')
-def logout():
-    session.pop('screen_name', None)
-    flash(u'You were signed out')
-    return redirect(request.referrer or url_for('index'))
+# @app.route('/oauth-authorized')
+# @twitter.authorized_handler
+# def oauth_authorized(resp):
+#     next_url = request.args.get('next') or url_for('index')
+#     if resp is None:
+#         flash(u'You denied the request to sign in.')
+#         return redirect(next_url)
 
-@app.route('/oauth-authorized')
-@twitter.authorized_handler
-def oauth_authorized(resp):
-    next_url = request.args.get('next') or url_for('index')
-    if resp is None:
-        flash(u'You denied the request to sign in.')
-        return redirect(next_url)
+#     access_token = resp['oauth_token']
+#     session['access_token'] = access_token
+#     session['screen_name'] = resp['screen_name']
 
-    access_token = resp['oauth_token']
-    session['access_token'] = access_token
-    session['screen_name'] = resp['screen_name']
+#     session['twitter_token'] = (
+#     resp['oauth_token'],
+#     resp['oauth_token_secret']
+#     )
 
-    session['twitter_token'] = (
-    resp['oauth_token'],
-    resp['oauth_token_secret']
-    )
-
-    return redirect(url_for('index'))
+#     return redirect(url_for('index'))
 
 @app.route('/policy')
 def policy():
