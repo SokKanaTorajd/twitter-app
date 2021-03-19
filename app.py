8bd65fe12dcc15a8d8b5c38d0cf2bd6a475eb463
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-03-19 21:17:30 +0700

    user authorization with tweepy module

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request, url_for
-import oauth2 as oauth
-import urllib.request
-import urllib.parse
-import urllib.error
-import json
+from flask import Flask, render_template, request, \
+    url_for, redirect, session
+import tweepy
 
 app = Flask(__name__)
-
 app.debug = False
 
 request_token_url = 'https://api.twitter.com/oauth/request_token'
@@ -38,95 +34,144 @@ def hello():
 def start():
     # note that the external callback URL must be added to the whitelist on
     # the developer.twitter.com portal, inside the app settings
-    app_callback_url = url_for('callback', _external=True)
+    callback_url = 'https://kecilin-twitter.herokuapp.com/callback'
+    auth = tweepy.OAuthHandler(
+                    app.config['APP_CONSUMER_KEY'], 
+                    app.config['APP_CONSUMER_SECRET'], 
+                    callback_url)
+    try:
+        url = auth.get_authorization_url()
+        return redirect(url)
+    except tweepy.TweepError:
+        print('Error! Failed to get request token.')
 
-    # Generate the OAuth request tokens, then display them
-    consumer = oauth.Consumer(
-        app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'])
-    client = oauth.Client(consumer)
-    resp, content = client.request(request_token_url, "POST", body=urllib.parse.urlencode({
-                                   "oauth_callback": app_callback_url}))
+    session.set('request_token', auth.request_token['oauth_token'])
 
-    if resp['status'] != '200':
-        error_message = 'Invalid response, status {status}, {message}'.format(
-            status=resp['status'], message=content.decode('utf-8'))
-        return render_template('error.html', error_message=error_message)
+    return render_template('start.html', 
+                            authorize_url=authorize_url, 
+                            oauth_token=auth.request_token['oauth_token'], 
+                            request_token_url=request_token_url)
 
-    request_token = dict(urllib.parse.parse_qsl(content))
-    oauth_token = request_token[b'oauth_token'].decode('utf-8')
-    oauth_token_secret = request_token[b'oauth_token_secret'].decode('utf-8')
+    # consumer = oauth.Consumer(
+    #     app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'])
+    # client = oauth.Client(consumer)
+    # resp, content = client.request(request_token_url, "POST", body=urllib.parse.urlencode({
+    #                                "oauth_callback": app_callback_url}))
 
-    oauth_store[oauth_token] = oauth_token_secret
-    return render_template('start.html', authorize_url=authorize_url, oauth_token=oauth_token, request_token_url=request_token_url)
+    # if resp['status'] != '200':
+    #     error_message = 'Invalid response, status {status}, {message}'.format(
+    #         status=resp['status'], message=content.decode('utf-8'))
+    #     return render_template('error.html', error_message=error_message)
+
+    # request_token = dict(urllib.parse.parse_qsl(content))
+    # oauth_token = request_token[b'oauth_token'].decode('utf-8')
+    # oauth_token_secret = request_token[b'oauth_token_secret'].decode('utf-8')
+
+    # oauth_store[oauth_token] = oauth_token_secret
+    # return render_template('start.html', authorize_url=authorize_url, oauth_token=oauth_token, request_token_url=request_token_url)
 
 
 @app.route('/callback')
 def callback():
-    # Accept the callback params, get the token and call the API to
-    # display the logged-in user's name and handle
-    oauth_token = request.args.get('oauth_token')
-    oauth_verifier = request.args.get('oauth_verifier')
-    oauth_denied = request.args.get('denied')
+    request_token = session['request_token']
+    verifier = request.GET.get('oauth_verifier')
 
-    # if the OAuth request was denied, delete our local token
-    # and show an error message
-    if oauth_denied:
-        if oauth_denied in oauth_store:
-            del oauth_store[oauth_denied]
-        return render_template('error.html', error_message="the OAuth request was denied by this user")
+    auth = tweepy.OAuthHandler(
+            config['APP_CONSUMER_KEY'],
+            config['APP_CONSUMER_SECRET'])
+    token = session.get('request_token')
+    auth.request_token = {
+        'oauth_token': token,
+        'oauth_token_secret': verifier}
+    try:
+        auth.get_access_token(verifier)
+        api = tweepy.API(auth)
+        user_verified = api.verify_credentials()
+        if user_verified:
+            screen_name = user_verified.screen_name
+            user_id = user_verified.id_str
+            name = user_verified.name
+            friends_count = user_verified.friends_count
+            statuses_count = user_verified.statuses_count
+            followers_count = user_verified.followers_count
 
-    if not oauth_token or not oauth_verifier:
-        return render_template('error.html', error_message="callback param(s) missing")
 
-    # unless oauth_token is still stored locally, return error
-    if oauth_token not in oauth_store:
-        return render_template('error.html', error_message="oauth_token not found locally")
+            return render_template('callback-success.html', 
+                                    screen_name=screen_name, 
+                                    user_id=user_id, name=name,
+                                    friends_count=friends_count, 
+                                    statuses_count=statuses_count, 
+                                    followers_count=followers_count, 
+                                    access_token_url=access_token_url)
+    except tweepy.TweepError:
+        print('Error! failed to access token')
 
-    oauth_token_secret = oauth_store[oauth_token]
 
-    # if we got this far, we have both callback params and we have
-    # found this token locally
+    # # Accept the callback params, get the token and call the API to
+    # # display the logged-in user's name and handle
+    # oauth_token = request.args.get('oauth_token')
+    # oauth_verifier = request.args.get('oauth_verifier')
+    # oauth_denied = request.args.get('denied')
 
-    consumer = oauth.Consumer(
-        app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'])
-    token = oauth.Token(oauth_token, oauth_token_secret)
-    token.set_verifier(oauth_verifier)
-    client = oauth.Client(consumer, token)
+    # # if the OAuth request was denied, delete our local token
+    # # and show an error message
+    # if oauth_denied:
+    #     if oauth_denied in oauth_store:
+    #         del oauth_store[oauth_denied]
+    #     return render_template('error.html', error_message="the OAuth request was denied by this user")
 
-    resp, content = client.request(access_token_url, "POST")
-    access_token = dict(urllib.parse.parse_qsl(content))
+    # if not oauth_token or not oauth_verifier:
+    #     return render_template('error.html', error_message="callback param(s) missing")
 
-    screen_name = access_token[b'screen_name'].decode('utf-8')
-    user_id = access_token[b'user_id'].decode('utf-8')
+    # # unless oauth_token is still stored locally, return error
+    # if oauth_token not in oauth_store:
+    #     return render_template('error.html', error_message="oauth_token not found locally")
 
-    # These are the tokens you would store long term, someplace safe
-    real_oauth_token = access_token[b'oauth_token'].decode('utf-8')
-    real_oauth_token_secret = access_token[b'oauth_token_secret'].decode(
-        'utf-8')
+    # oauth_token_secret = oauth_store[oauth_token]
 
-    # Call api.twitter.com/1.1/users/show.json?user_id={user_id}
-    real_token = oauth.Token(real_oauth_token, real_oauth_token_secret)
-    real_client = oauth.Client(consumer, real_token)
-    real_resp, real_content = real_client.request(
-        show_user_url + '?user_id=' + user_id, "GET")
+    # # if we got this far, we have both callback params and we have
+    # # found this token locally
 
-    if real_resp['status'] != '200':
-        error_message = "Invalid response from Twitter API GET users/show: {status}".format(
-            status=real_resp['status'])
-        return render_template('error.html', error_message=error_message)
+    # consumer = oauth.Consumer(
+    #     app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'])
+    # token = oauth.Token(oauth_token, oauth_token_secret)
+    # token.set_verifier(oauth_verifier)
+    # client = oauth.Client(consumer, token)
 
-    response = json.loads(real_content.decode('utf-8'))
+    # resp, content = client.request(access_token_url, "POST")
+    # access_token = dict(urllib.parse.parse_qsl(content))
 
-    friends_count = response['friends_count']
-    statuses_count = response['statuses_count']
-    followers_count = response['followers_count']
-    name = response['name']
+    # screen_name = access_token[b'screen_name'].decode('utf-8')
+    # user_id = access_token[b'user_id'].decode('utf-8')
 
-    # don't keep this token and secret in memory any longer
-    del oauth_store[oauth_token]
+    # # These are the tokens you would store long term, someplace safe
+    # real_oauth_token = access_token[b'oauth_token'].decode('utf-8')
+    # real_oauth_token_secret = access_token[b'oauth_token_secret'].decode(
+    #     'utf-8')
 
-    return render_template('callback-success.html', screen_name=screen_name, user_id=user_id, name=name,
-                           friends_count=friends_count, statuses_count=statuses_count, followers_count=followers_count, access_token_url=access_token_url)
+    # # Call api.twitter.com/1.1/users/show.json?user_id={user_id}
+    # real_token = oauth.Token(real_oauth_token, real_oauth_token_secret)
+    # real_client = oauth.Client(consumer, real_token)
+    # real_resp, real_content = real_client.request(
+    #     show_user_url + '?user_id=' + user_id, "GET")
+
+    # if real_resp['status'] != '200':
+    #     error_message = "Invalid response from Twitter API GET users/show: {status}".format(
+    #         status=real_resp['status'])
+    #     return render_template('error.html', error_message=error_message)
+
+    # response = json.loads(real_content.decode('utf-8'))
+
+    # friends_count = response['friends_count']
+    # statuses_count = response['statuses_count']
+    # followers_count = response['followers_count']
+    # name = response['name']
+
+    # # don't keep this token and secret in memory any longer
+    # del oauth_store[oauth_token]
+
+    # return render_template('callback-success.html', screen_name=screen_name, user_id=user_id, name=name,
+    #                        friends_count=friends_count, statuses_count=statuses_count, followers_count=followers_count, access_token_url=access_token_url)
 
 
 @app.route('/policy')
