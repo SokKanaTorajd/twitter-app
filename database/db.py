decd76685d350892a9675e0f575e8d49af5448eb
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-03-20 14:51:11 +0700

    store tokens and call user_access

--- database/db.py
+++ database/db.py
@@ -1,12 +1,25 @@
 from pymongo import MongoClient
+import os
 
 class DB(object):
 
     def __init__(self):
-        self.client = MongoClient()
-        self.database = "kecilin_intern"
+        # self.client = MongoClient()
+        self.client = MongoClient(os.environ.get('MONGODB_URI'))
+        self.database = "kecilin-intern"
 
-    def insertDB(self, data):
+    def insertAuthorizedUser(self, data):
         db = self.client[self.database]
-        collection = "twitter_data"
+        collection = "twitter-app"
+        return db[collection].insert_one(data)
+    
+    def getUserAccess(self, id):
+        db = self.client[self.database]
+        collection = "twitter-app"
+        result = db[collection].find_one('user_id': id)
+        return result['access']
+
+    def insertTweet(self, data):
+        db = self.client[self.database]
+        collection = "twitter-data"
         return db[collection].insert_one(data)
