4c34140fbbd1b8bd6b2e70cb17897e436102e6c9
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-04-01 14:50:10 +0700

    add api example

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
 from flask import Flask, flash, render_template, request, \
     url_for, redirect, jsonify
 # from database.db import DB
+from flask_restplus import Api, Resource, fields
 import tweepy
 
 app = Flask(__name__)
 app.debug = False
+app_api = Api(app, version='1.0', title='Integrated Media - Twitter API',
+    description='Kecilin Integrated Media API v1.0')
+
+twitter_user = api.namespace('user', description='Socmed Twitter API')
+user_model = twitter_user.model('IMTwitter', {
+    'user_id' : fields.String(required=True, description='User id'),
+    'screen_name' : fields.String(required=True, description='Twitter Username'),
+    'name' : fields.String(required=True, description='Twitter name'),
+    'description' : fields.String(required=True, description='User bio description'),
+    'statuses_count' : fields.Integer(required=True, description='User Statuses Count'),
+    'friends_count' : fields.Integer(required=True, description='User Followings Count'),
+    'followers_count' : fields.Integer(required=True, description='User Followers Count'),
+    'favourites_count': fields.Integer(required=True, description='User Likes Count'),
+    'profile_image' : fields.String(required=True, description='User profile image link')
+}) 
+
+users = [
+    {
+        'user_id': '1117409047261540353', 
+        'screen_name': 'radiputra49', 
+        'name': 'radiputra49', 
+        'description': 'Just for fun', 
+        'statuses_count': 14, 
+        'friends_count': 7, 
+        'followers_count': 0, 
+        'favourites_count': 3, 
+        'profile_image': 'http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'
+    },
+    {
+        'user_id': '2880366295', 
+        'screen_name': 'devtorajd', 
+        'name': 'helios', 
+        'description': 
+        'Master of None - Jack of All Trades', 
+        'statuses_count': 5413, 
+        'friends_count': 235, 
+        'followers_count': 151, 
+        'favourites_count': 6599, 
+        'profile_image': 'http://pbs.twimg.com/profile_images/1377249145316990982/ECR_CkiK_normal.jpg'
+        }]
 
 request_token_url = 'https://api.twitter.com/oauth/request_token'
 access_token_url = 'https://api.twitter.com/oauth/access_token'
@@ -49,7 +90,7 @@ def callback():
 
         try:
             auth.get_access_token(request.args.get('oauth_verifier'))
-            # print('Token is Authorized!')
+
         except tweepy.TweepError as e:
             error_message = 'Invalid response, {message}'.format(message=e)
             return render_template('error.html', error_message=error_message)
@@ -59,27 +100,29 @@ def callback():
         user_verified = api.verify_credentials()
 
         if user_verified:
-            tokens = {
-                'token': auth.access_token, 
-                'token_secret': auth.access_token_secret}
 
             user_info = {
                 'user_id' : user_verified.id_str,
                 'screen_name' : user_verified.screen_name,
                 'name' : user_verified.name,
+                'description' : user_verified.description,
                 'statuses_count' : user_verified.statuses_count,
                 'friends_count' : user_verified.friends_count,
                 'followers_count' : user_verified.followers_count,
-                'favorites_count': user_verified.favourites_count,
+                'favourites_count': user_verified.favourites_count,
                 'profile_image' : user_verified.profile_image_url}
-            
-            data = {
-                'access': tokens,
-                'user': user_info}
 
-            return jsonify(data)
+            return jsonify(user_info)
 
         return jsoniy({"error": "access unauthorized"})
+
+@twitter_user.route('/user-twitter')
+class UserData(Resource):
+    @twitter_user.doc('get_user_model')
+    @twitter_user.marshal_with(user_model)
+    def get(self):
+        return users
+
             # db.insertAuthorizedUser(data)
 
             # return render_template('callback-success.html', 
